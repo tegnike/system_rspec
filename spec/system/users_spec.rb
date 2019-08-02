34d228d0dcd9b128354b9a623a5f91327757d193
belion-freee <belion.freee@gmail.com>
2019-08-02 13:17:51 +0900

    fix test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe 'Users', type: :system, js: true do
   before do
-    @user = User.create!(name: 'いとう')
+    @user = User.create!(name: 'Test User')
   end
 
   it 'completes yubinbango automatically with JS' do
     # User編集画面を開く
-    visit edit_user_path(@user)
+    visit user_path(@user)
 
     # Nameに"いとう"が入力されていることを検証する
-    expect(page).to have_field '名前', with: 'いとう'
+    expect(page).to have_content @user.name
   end
 end
