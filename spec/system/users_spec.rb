cebb20a1e9bb937c084d239c8999e2309d8444dc
belion-freee <belion.freee@gmail.com>
2019-08-02 14:23:58 +0900

    fix Test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system, js: true do
-  before do
-    @user = User.create!(name: 'Test User')
-  end
+  it 'succcess CRUD' do
+    visit users_path
+    expect(page).to have_css('h1', text: 'Users')
 
-  it 'completes yubinbango automatically with JS' do
-    # User編集画面を開く
-    visit user_path(@user)
+    click_on 'New User'
+    fill_in "Name", with: 'Created User'
+    click_on 'Create User'
 
-    # Nameに"いとう"が入力されていることを検証する
-    expect(page).to have_content @user.name
+    expect(page).to have_content "User was successfully created."
+
+    click_on 'Edit'
+    fill_in "Name", with: 'Updated User'
+    click_on 'Update User'
+
+    expect(page).to have_content "User was successfully updated."
+
+    click_on 'Back'
+    click_on 'Show'
+    expect(page).to have_content 'Updated User'
+
+    click_on 'Back'
+
+    expect(page).to have_css('h1', text: 'Users')
+    expect(page).to have_css('td', text: 'Updated User')
+
+    link = find_link 'Destroy'
+    expect(link['data-confirm']).to eq 'Are you sure?'
+
+    page.accept_confirm 'Are you sure?' do
+      click_on 'Destroy'
+    end
+
+    expect(page).to have_css('h1', text: 'Users')
+    expect(page).to have_no_css('td', text: 'Updated User')
   end
 end
